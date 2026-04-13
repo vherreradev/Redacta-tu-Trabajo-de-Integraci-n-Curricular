11f2288dd091b7c4a4082b2364fd62b84f34f34f
vherreradev <vherrera@unl.edu.ec>
2026-04-13 13:18:08 -0500

    feature: fix my project on github cloud

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
